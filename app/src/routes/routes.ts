2deb370908737bceca4763f0379aaa497d96bb47
FedericoPaolucci <s1116751@studenti.univpm.it>
2024-08-29 17:20:25 +0200

    controller e routes CRUD

--- app/src/routes/routes.ts
+++ app/src/routes/routes.ts
@@ -10,6 +10,7 @@ import Violation from '../models/Violation';
 import Limit from '../models/Limit';
 
 // Controllers
+import CRUDController from '../controllers/CRUDController'
 
 // Middlewares
 
@@ -20,45 +21,45 @@ router.get("/", (req, res) => res.send("TEST"));
 
 // test CRUD
 // User
-router.post("/api/users", async (req, res) => { const user = await User.create(req.body); return res.status(201).json(user);});
-router.get("/api/users", async (req, res) => { const users = await User.findAll(); return res.status(200).json(users);});
-router.delete("/api/users/:id", async (req, res) => { await User.destroy({ where: { id: req.params.id } }); return res.status(204).send();});
-router.put("/api/users/:id", async (req, res) => { await User.update(req.body, { where: { id: req.params.id } }); const updatedUser = await User.findByPk(req.params.id); return res.status(200).json(updatedUser);});
+router.post("/api/users", async (req, res) => CRUDController.createRecord(User, req, res));
+router.get("/api/users", async (req, res) => CRUDController.readOneRecord(User, req, res));
+router.delete("/api/users/:id", async (req, res) => CRUDController.deleteRecord(User, req, res));
+router.put("/api/users/:id", async (req, res) => CRUDController.updateRecord(User, req, res));
 
 // Vehicle
-router.post("/api/vehicles", async (req, res) => { const vehicle = await Vehicle.create(req.body); return res.status(201).json(vehicle);});
-router.get("/api/vehicles", async (req, res) => { const vehicles = await Vehicle.findAll(); return res.status(200).json(vehicles);});
-router.delete("/api/vehicles/:plate", async (req, res) => { await Vehicle.destroy({ where: { plate: req.params.plate } }); return res.status(204).send();});
-router.put("/api/vehicles/:plate", async (req, res) => { await Vehicle.update(req.body, { where: { plate: req.params.plate } }); const updatedVehicle = await Vehicle.findByPk(req.params.plate); return res.status(200).json(updatedVehicle);});
+router.post("/api/vehicles", async (req, res) => CRUDController.createRecord(Vehicle, req, res));
+router.get("/api/vehicles", async (req, res) => CRUDController.readOneRecord(Vehicle, req, res));
+router.delete("/api/vehicles/:plate", async (req, res) => CRUDController.deleteRecord(Vehicle, req, res));
+router.put("/api/vehicles/:plate", async (req, res) => CRUDController.updateRecord(Vehicle, req, res));
 
 // Gateway
-router.post("/api/gateways", async (req, res) => { const gateway = await Gateway.create(req.body); return res.status(201).json(gateway);});
-router.get("/api/gateways", async (req, res) => { const gateways = await Gateway.findAll(); return res.status(200).json(gateways);});
-router.delete("/api/gateways/:id", async (req, res) => { await Gateway.destroy({ where: { id: req.params.id } }); return res.status(204).send();});
-router.put("/api/gateways/:id", async (req, res) => { await Gateway.update(req.body, { where: { id: req.params.id } }); const updatedGateway = await Gateway.findByPk(req.params.id); return res.status(200).json(updatedGateway);});
+router.post("/api/gateways", async (req, res) => CRUDController.createRecord(Gateway, req, res));
+router.get("/api/gateways", async (req, res) => CRUDController.readOneRecord(Gateway, req, res));
+router.delete("/api/gateways/:id", async (req, res) => CRUDController.deleteRecord(Gateway, req, res));
+router.put("/api/gateways/:id", async (req, res) => CRUDController.updateRecord(Gateway, req, res));
 
 // Segment
-router.post("/api/segments", async (req, res) => { const segment = await Segment.create(req.body); return res.status(201).json(segment);});
-router.get("/api/segments", async (req, res) => { const segments = await Segment.findAll(); return res.status(200).json(segments);});
-router.delete("/api/segments/:id", async (req, res) => { await Segment.destroy({ where: { id_gateway1: req.params.id } }); return res.status(204).send();});
-router.put("/api/segments/:id", async (req, res) => { await Segment.update(req.body, { where: { id_gateway1: req.params.id } }); const updatedSegment = await Segment.findByPk(req.params.id); return res.status(200).json(updatedSegment);});
+router.post("/api/segments", async (req, res) => CRUDController.createRecord(Segment, req, res));
+router.get("/api/segments", async (req, res) => CRUDController.readOneRecord(Segment, req, res));
+router.delete("/api/segments/:id", async (req, res) => CRUDController.deleteRecord(Segment, req, res));
+router.put("/api/segments/:id", async (req, res) => CRUDController.updateRecord(Segment, req, res));
 
 // Transit
-router.post("/api/transits", async (req, res) => { const transit = await Transit.create(req.body); return res.status(201).json(transit);});
-router.get("/api/transits", async (req, res) => { const transits = await Transit.findAll(); return res.status(200).json(transits);});
-router.delete("/api/transits/:id", async (req, res) => { await Transit.destroy({ where: { id: req.params.id } }); return res.status(204).send();});
-router.put("/api/transits/:id", async (req, res) => { await Transit.update(req.body, { where: { id: req.params.id } }); const updatedTransit = await Transit.findByPk(req.params.id); return res.status(200).json(updatedTransit);});
+router.post("/api/transits", async (req, res) => CRUDController.createRecord(Transit, req, res));
+router.get("/api/transits", async (req, res) => CRUDController.readOneRecord(Transit, req, res));
+router.delete("/api/transits/:id", async (req, res) => CRUDController.deleteRecord(Transit, req, res));
+router.put("/api/transits/:id", async (req, res) => CRUDController.updateRecord(Transit, req, res));
 
 // Violation
-router.post("/api/violations", async (req, res) => { const violation = await Violation.create(req.body); return res.status(201).json(violation);});
-router.get("/api/violations", async (req, res) => { const violations = await Violation.findAll(); return res.status(200).json(violations);});
-router.delete("/api/violations/:id", async (req, res) => { await Violation.destroy({ where: { id: req.params.id } }); return res.status(204).send();});
-router.put("/api/violations/:id", async (req, res) => { await Violation.update(req.body, { where: { id: req.params.id } }); const updatedViolation = await Violation.findByPk(req.params.id); return res.status(200).json(updatedViolation);});
+router.post("/api/violations", async (req, res) => CRUDController.createRecord(Violation, req, res));
+router.get("/api/violations", async (req, res) => CRUDController.readOneRecord(Violation, req, res));
+router.delete("/api/violations/:id", async (req, res) => CRUDController.deleteRecord(Violation, req, res));
+router.put("/api/violations/:id", async (req, res) => CRUDController.updateRecord(Violation, req, res));
 
 // Limit
-router.post("/api/limits", async (req, res) => { const limit = await Limit.create(req.body); return res.status(201).json(limit);});
-router.get("/api/limits", async (req, res) => { const limits = await Limit.findAll(); return res.status(200).json(limits);});
-router.delete("/api/limits/:vehicle_type", async (req, res) => { await Limit.destroy({ where: { vehicle_type: req.params.vehicle_type } }); return res.status(204).send();});
-router.put("/api/limits/:vehicle_type", async (req, res) => { await Limit.update(req.body, { where: { vehicle_type: req.params.vehicle_type } }); const updatedUser = await Limit.findByPk(req.params.vehicle_type); return res.status(200).json(updatedUser);});
+router.post("/api/limits", async (req, res) => CRUDController.createRecord(Limit, req, res));
+router.get("/api/limits", async (req, res) => CRUDController.readOneRecord(Limit, req, res));
+router.delete("/api/limits/:vehicle_type", async (req, res) => CRUDController.deleteRecord(Limit, req, res));
+router.put("/api/limits/:vehicle_type", async (req, res) => CRUDController.updateRecord(Limit, req, res));
 
 export default router;
