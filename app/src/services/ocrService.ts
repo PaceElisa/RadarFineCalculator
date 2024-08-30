430a316271f762eaa666cfc2137f91ff47843a4f
Elisa Pace <pace.elisa1@gmail.com>
2024-08-30 12:07:41 +0200

    Add services with ocrServices

--- app/src/services/ocrService.ts
+++ app/src/services/ocrService.ts
@@ -1,18 +1,23 @@
 import * as tesseract_ocr from 'node-tesseract-ocr';
 
+//setta i parametri da passare
 const config = {
-    lang: "eng",
-    oem: 3,
-    psm: 3,
-}
+    lang: "eng",  // Lingua del modello di OCR
+    oem: 3,      // Utilizza automaticamente il motore più adatto (spesso LSTM, che è avanzato e accurato)
+    psm: 6,      // Assume un singolo blocco uniforme di testo
+  }
 
-export const recognizeTextFromImage = async(imagePath: string) => {
+export const recognizeTextFromImage = async(imagePath: string): Promise<string> => {
     try{
-        const text = await tesseract_ocr.recognize(imagePath, config)
-        console.log("Result:", text)
+        const text = await tesseract_ocr.recognize(imagePath, config);
+        return text.trim();
+        //console.log("Result:", text) //for initial testing
     }catch (error){
         console.error("Error recognitzing text:", error);
         throw new Error("OCR recognition failed");
     }
 } 
-recognizeTextFromImage("prova1.png");
+/*//Prova
+const img1 ="https://production-media.paperswithcode.com/thumbnails/task/b4482a02-2e61-4725-a6c2-6cfcf6c7c31f.jpg";
+recognizeTextFromImage(img1);
+*/
--- app/src/services/prova.js
+++ app/src/services/prova.js
@@ -1,18 +1,25 @@
 const tesseract = require("node-tesseract-ocr")
+const path = require("path");
 
-const img = "https://tesseract.projectnaptha.com/img/eng_bw.png"
+
+const img = "https://miro.medium.com/v2/resize:fit:828/format:webp/0*pVWZKQthOggVOr0S.jpeg";
+const img1 ="https://production-media.paperswithcode.com/thumbnails/task/b4482a02-2e61-4725-a6c2-6cfcf6c7c31f.jpg";//red car IT20BOM
+//const img = path.join(__dirname, "prova1.png");
+
 
 const config = {
-    lang: "eng",
-    oem: 1,
-    psm: 3,
-  }
+  lang: "eng",  // Lingua del modello di OCR
+  oem: 3,      // Utilizza il motore LSTM, che è avanzato e accurato
+  psm: 6,      // Presuppone una singola riga di testo (ideale per targhe)
+}
+
 
 tesseract
-  .recognize(img, config)
+  .recognize(img1, config)
   .then((text) => {
     console.log("Result:", text)
   })
   .catch((error) => {
     console.log(error.message)
   })
+  
