136ee345ff5c2031d193c86492d268abd7cf85c2
Elisa Pace <pace.elisa1@gmail.com>
2024-08-31 16:52:55 +0200

    Add in check.ts method checkImage

--- app/src/middleware/check.ts
+++ app/src/middleware/check.ts
@@ -17,10 +17,10 @@ import { Model, ModelStatic } from "sequelize";
 //Import services OCR
 import { recognizeTextFromImage } from "../services/ocrService";
 
-const MessageFact: MessageFactory = new MessageFactory();
+const messageFactory: MessageFactory = new MessageFactory();
 
  export interface ICustomRequest extends Request{
-    message?: IMessage[];
+    messages?: IMessage[];
     imageUpload?: boolean;
 }
 
@@ -50,18 +50,33 @@ class generalCheck{
     //Check if an image is provided and try to identify the text (plate) present
      async checkImage(req:ICustomRequest, res: Response, next:NextFunction){
         req.imageUpload = false; // flag to pass in the middleware for checking if an image is provied
-        
+        //Create an Info message if not already presente in the requeste    
+        if (!req.messages) {
+            req.messages = [];
+        }
+
         if (req.file) {
             try {
             const imagePath = req.file.path;
+            //Update the path image in the body 
             req.body.img_route = imagePath;
             const recognizedText = await recognizeTextFromImage(imagePath);
-
-            req.body.pl
+            
+            //Update the license plate in the body
+            req.body.plate = recognizedText;
+            req.imageUpload = true;
+            
+            
+            req.messages.push(messageFactory.createMessage(HttpStatus.OK, "Analisi dell'immagine è andata a buon fine."));
             
           
-            }catch{}
+            }catch(error){
+                next(messageFactory.createMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Qualcosa è andato storto. Analisi dell'immagine non è andata a buon fine."));
+
+            }
         }
+        req.messages.push(messageFactory.createMessage(HttpStatus.OK, "Immagine opzionale non inserita"));
+
 
         next();
     }
