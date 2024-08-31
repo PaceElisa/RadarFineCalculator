57bb61d887cff52d8dfe13ffc26ac0f3bdc9da1d
Elisa Pace <pace.elisa1@gmail.com>
2024-08-31 02:34:24 +0200

    add new method to do in validateDate middleware

--- app/src/middleware/validateData.ts
+++ app/src/middleware/validateData.ts
@@ -9,11 +9,23 @@ import Transit from "../models/Transit";
 import User from "../models/User";
 import Violation from "../models/Violation";
 
+//Import factory
+import { MessageFactory } from "../factory/Messages";
+import { HttpStatus } from "../factory/Messages";
+
+const MessageFact: MessageFactory = new MessageFactory();
+
 //create class validateData
 class validateData{
 
     validateRequestId(req:Request, res: Response, next:NextFunction){
-        //TO DO ... 
+        const {id} = req.params;
+
+        //Check if the id is specified and a valid number
+        if(!id || isNaN(Number(id))){
+            return next(MessageFact.createMessage(HttpStatus.BAD_REQUEST, "ID Not Valid"));
+        }
+        
         next();
     }
 
@@ -37,4 +49,32 @@ class validateData{
         next();
     }
 
+    validateVehicleDataUpdate(req:Request, res: Response, next:NextFunction){
+        //TO DO ... 
+        next();
+    }
+
+    validateSegmentDataUpdate(req:Request, res: Response, next:NextFunction){
+        //TO DO ... 
+        next();
+    }
+
+    validateGatewayDataUpdate(req:Request, res: Response, next:NextFunction){
+        //TO DO ... 
+        next();
+    }
+
+    validateTransitDataUpdate(req:Request, res: Response, next:NextFunction){
+        //TO DO ... 
+        next();
+    }
+
+    validatePlate(req:Request, res: Response, next:NextFunction){
+        //TO DO ... 
+        next();
+    }
+
+
 }
+//Export an instance of validateDAta
+export default new validateData();
